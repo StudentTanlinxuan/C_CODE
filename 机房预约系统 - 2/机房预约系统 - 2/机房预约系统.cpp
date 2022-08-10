ee568a260678cb85efbbd04abbd8cf433dd59008
StudentTanlinxuan <2914442315@qq.com>
2022-08-10 20:55:32 +0800

    C++(机房预约系统 - 2)

---
+++ 机房预约系统 - 2/机房预约系统 - 2/机房预约系统.cpp
@@ -0,0 +1,169 @@
+#include<iostream>
+#include"student.h"
+#include"teacher.h"
+#include"manager.h"
+#include<fstream>
+#include"globalFile.h" // global 全球的
+
+using namespace std;
+
+void LoginIn(string fileName, int type) // fileName - 操作的文件名  type - 登录的身份(1代表学生，2代表老师，3代表管理员)
+{
+	Identity* person = NULL;
+
+	ifstream ifs;
+	ifs.open(fileName, ios::in); // ios::in 读文件的形式
+
+	// 文件不存在
+	if (!ifs.is_open())
+	{
+		cout << "文件不存在" << endl;
+		ifs.close();
+		return;
+	}
+
+	int id = 0;
+	string name;
+	string pwd;
+
+	if (type == 1)// 学生登陆
+	{
+		cout << "请输入你的学号>:" << endl;
+		cin >> id;
+	}
+	else if (type == 2)// 教师登录
+	{
+		cout << "请输入你的职工号>:" << endl;
+		cin >> id;
+	}
+	
+	cout << "请输入用户名>:" << endl;
+	cin >> name;
+	cout << "请输入密码" << endl;
+	cin >> pwd;
+
+	if (type == 1)
+	{
+		// 学生验证
+		int fId = 0;
+		string fName;
+		string fPwd;
+
+		while (ifs >> fId && ifs >> fName && ifs >> fPwd)
+		{
+			// cout << fId << fName << fPwd << endl;
+
+			if (id == fId && name == fName && pwd == fPwd)
+			{
+				cout << "学生验证登录成功!" << endl;
+				system("pause");
+				system("cls");
+				person = new Student(id, name, pwd);
+
+				return;
+			}
+		}
+	}
+	else if (type == 2)
+	{
+		// 老师验证
+		int fId = 0;
+		string fName;
+		string fPwd;
+
+		while (ifs >> fId && ifs >> fName && ifs >> fPwd)
+		{
+			if (id == fId && name == fName && pwd == fPwd)
+			{
+				cout << "老师验证登录成功!" << endl;
+				system("pause");
+				system("cls");
+				person = new Teacher(id, name, pwd);
+
+				return;
+			}
+		}
+	}
+	else if (type == 3)
+	{
+		// 管理员验证
+		string fName;
+		string fPwd;
+
+		while (ifs >> fName && ifs >> fPwd)
+		{
+			if (name == fName && pwd == fPwd)
+			{
+				cout << "验证登录成功!" << endl;
+				// 登录成功后按任意键进入管理员界面
+				system("pause");
+				system("cls");
+				// 创建管理员对象
+				person = new Manager(name, pwd);
+
+				return;
+			}
+		}
+	}
+
+	cout << "验证失败" << endl;
+
+	system("pause");
+	system("cls");
+	return;
+}
+
+int main()
+{
+	int select = 0;
+	do
+	{
+		cout << "================= 欢迎来到小谈机房预约系统 ===================" << endl;
+
+		cout << "\t\t ---------------------------------- \n";
+		cout << "\t\t|                                  |\n";
+		cout << "\t\t|          1. 学生代表             |\n";
+		cout << "\t\t|                                  |\n";
+		cout << "\t\t|          2. 老    师             |\n";
+		cout << "\t\t|                                  |\n";
+		cout << "\t\t|          3. 管 理 员             |\n";
+		cout << "\t\t|                                  |\n";
+		cout << "\t\t|          0. 退    出             |\n";
+		cout << "\t\t|                                  |\n";
+		cout << "\t\t ---------------------------------- \n";
+
+		cout << "请输入你的选择>:" << endl;
+		cin >> select;
+		switch (select)
+		{
+		case 1:
+			// 学生代表
+			LoginIn(STUDENT_FILE, 1);
+			break;
+		case 2:
+			// 老师
+			LoginIn(TEACHER_FILE, 2);
+			break;
+		case 3:
+			// 管理员
+			LoginIn(ADMIN_FILE, 3);
+			break;
+		case 0:
+			// 退出
+			cout << "欢迎下次使用！" << endl;
+			system("pause");
+			return 0;
+			break;
+		default:
+			cout << "输入错误，请重新输入！" << endl;
+			system("pause");
+			system("cls");
+			break;
+		}
+	} while (select);
+	// 这个循环可以使用do while(select)，也可以使用while死循环
+
+	system("pause");
+
+	return 0;
+}
